aeb4353cc78929988bed0afc1b8e5c471148591a
nakayama <nakayama@jurabi.jp>
2022-04-21 11:07:45 +0900

    [WIP 3]優先度の機能追加しました。

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // import firebase from 'firebase'
 [
-    { "id": 1, "projectName": "プロ1", "description": "説明1", "state": 0 },
-    { "id": 2, "projectName": "プロ2", "description": "説明2", "state": 0 }
+    { "id": 1, "priority": 0, "projectName": "プロ1", "description": "説明1", "state": 0 },
+    { "id": 2, "priority": 0, "projectName": "プロ2", "description": "説明2", "state": 0 }
 ]
 const app = new Vue({
     el: '#app',
@@ -16,29 +16,33 @@ const app = new Vue({
       // 選択している options の value を記憶するためのデータ
       // 初期値を「-1」つまり「すべて」にする
       current: -1,
-
+      //優先度のselect
+      selectedItem: [],
     },
     methods: {
       // 使用するメソッド
         // ToDo 追加の処理
         doAdd: function(event, value) {
             // ref で名前を付けておいた要素を参照
+            var priority = this.$refs.priority
             var projectName = this.$refs.projectName
             var description = this.$refs.description
             // 入力がなければ何もしないで return
             if (!projectName.value.length || !description.value.length) {
               return
             }
-            // { 新しいID, コメント, 作業状態 }
+            // { 新しいID, 優先度, project名, 説明, 作業状態 }
             // というオブジェクトを現在の todos リストへ push
             // 作業状態「state」はデフォルト「作業中=0」で作成
             this.todos.push({
               id: todoStorage.uid++,
+              priority: priority.value,
               projectName: projectName.value,
               description: description.value,
               state: 0
             })
             // フォーム要素を空にする
+            priority.value = 
             projectName.value = ''
             description.value = ''
         },
@@ -78,7 +82,13 @@ const app = new Vue({
           },
           // deep オプションでネストしているデータも監視できる
           deep: true
-        }
+        },
+        //フォームを空にする
+        selectedItem(value) {
+          if (!value) {
+            this.priority = "";
+          }
+        },
     },
     created() {
         // インスタンス作成時に自動的に fetch() する
